36d6788b4af004d8d1e215aa850d16d0c862965b
boring-km <kms0644804@naver.com>
2021-09-28 12:26:39 +0900

    [master] 안드로이드에서 앱 실행 확인함

---
+++ android/app/src/main/kotlin/com/boringkm/flutter_app_executor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.boringkm.flutter_app_executor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:external_app_launcher/external_app_launcher.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LauncherApp());
+}
+
+class LauncherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '앱 실행기',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Flutter App Test", style: TextStyle(color: Colors.blue, fontSize: 30),),
+            ElevatedButton(onPressed: () async {
+              bool result = await LaunchApp.isAppInstalled(androidPackageName: '안드로이드 패키지명', iosUrlScheme: '');
+              if (result) LaunchApp.openApp(androidPackageName: '안드로이드 패키지명', iosUrlScheme: '', appStoreLink: '', openStore: false);
+            }, child: Text("이동할 앱 1")),
+            ElevatedButton(onPressed: () {}, child: Text("이동할 앱 2")),
+          ],
+        ),
+      ),
+    );
+  }
+}
